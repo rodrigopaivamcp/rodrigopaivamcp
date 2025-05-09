af09acec40e20decc0759d275f13e7169d5e35a9
rodrigopaivamcp <rodrigo.mcpaiva@sempreceub.com>
2025-05-09 17:41:05 -0300

    Update programa.c

--- programa.c
+++ programa.c
@@ -2,9 +2,9 @@
 int main(int argc, char **argv[])
 {
 
-float A, B, C;
-    scanf("%f%f%f", &A, &B, &C);
-    printf("MEDIA = %.1f\n",
+double A, B, C;
+    scanf("%lf%lf%lf", &A, &B, &C);
+    printf("MEDIA = %.1lf\n",
     ((A*2)+(B*3)+(C*5)/10));
 
     return 0;
